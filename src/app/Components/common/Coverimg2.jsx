1ebd489fdb07021a06412795013181962431ec1f
Tonmoy-Roy <roytonmoy901@gmail.com>
2025-11-07 10:35:16 +0600

    pages : olight collection added

---
+++ src/app/Components/common/Coverimg2.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image';
+import Button from './Button';
+
+const Coverimg2 = ({ imagePath, title, breadcrumb, breadcrumb2 }) => {
+    return (
+        <div>
+            <section className="relative w-full h-[45vh] flex items-center bg-gray-200 overflow-hidden">
+                <Image
+                    src={imagePath}
+                    alt={title}
+                    fill
+                    priority
+                    className="absolute inset-0 object-cover"
+                />
+                <div className="absolute inset-0 bg-black/30"></div>
+                {/* Text Content */}
+                <div className="relative ml-40 z-10 text-white md:w-[41vw] text-left">
+                    <p>{breadcrumb}</p>
+                    <h1 className="text-2xl md:text-3xl font-semibold mt-3 mb-5">{title}</h1>
+                    <p className="text-sm md:text-base text-gray-200 tracking-wider mb-5">
+                        {breadcrumb2}
+                    </p>
+                    <Button variant="white"></Button>
+                </div>
+
+            </section>
+        </div>
+    );
+};
+
+export default Coverimg2;
--- src/app/pages/page.jsx
+++ src/app/pages/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Coverimg1 from '../Components/common/Coverimg1';
+import Coverimg2 from '../Components/common/Coverimg2';
+import Footer from '../Components/layout/Footer';
 
 const page = () => {
     return (
@@ -16,7 +18,7 @@ const page = () => {
                     breadcrumb="Olight is building the world’s most progressive jewellery platform – a place connecting the dots between old school craftsmanship and new ways of thinking. Through our online platform, we connect you with the most creative independent jewellers from around the globe."
                 />
             </div>
-            <div className='md:flex'>
+            <div className='md:flex mb-20'>
                 <div className='mr-5'>
                     <img className='rounded-2xl md:h-[60vh] md:w-[30vw]' src="/images/OlightAllImage/4ProductPage/15.jpg" alt="" />
                 </div>
@@ -28,7 +30,23 @@ const page = () => {
                     <p>Jewelry is a form of personal adornment that has been worn by humans for thousands of years. It is typically made from precious metals such as gold, silver, and platinum, as well as precious stones such as diamonds, rubies, emeralds, and sapphires. Jewelry can be worn for a variety of reasons, including as a form of self-expression, as a symbol of wealth or status, as a religious or cultural symbol, or as a way to commemorate a special occasion or relationship. When buying jewelry, it’s important to consider factors such as the quality of the materials used, the design and style of the piece, and the reputation of the jeweler. It’s also important to care for your jewelry properly to keep it looking its best, such as by storing it in a safe place, avoiding exposure to chemicals and moisture, and cleaning it regularly. Jewelry has been used throughout history for a variety of purposes, including as a symbol of wealth and social status, as a form of religious or cultural expression, and as a way to enhance one’s personal appearance. In many cultures, jewelry is also used as a form of protection against evil or harmful forces.</p>
                 </div>
             </div>
-            <div className='min-h-screen'></div>
+            <div>
+                <div className="w-screen relative left-1/2 -ml-[51vw] mb-20">
+                    <Coverimg2
+                        imagePath="/images/OlightAllImage/4ProductPage/1.jpg"
+                        breadcrumb={<>Olight Collection</>}
+                        title={
+                            <>
+                                Discover Your Signature <br />
+                                Sparkle with New Collection
+                            </>
+                        }
+
+                        breadcrumb2={<>Exceptional Handcrafted Design to Enhance The Magnificent <br /> Glow</>}
+                    />
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
